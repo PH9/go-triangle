812fe57301b07ebf9100317061c347a6f8187581
Wasith Theerapattrathamrong - Acer <phai@live.com>
2015-10-01 11:51:00 +0700

    move showHelp to function

--- triangle.go
+++ triangle.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
+	if ShowHelp() {
+		return
+	}
+}
+
+func ShowHelp() bool {
 	if len(os.Args) != 4 {
 		fmt.Println("Please enter 3 parameters for 3 sides of triangle.")
 		fmt.Println("  eg. " + os.Args[0] + " 3 4 5")
-		return
+		return true
 	}
-
+	return false
 }
