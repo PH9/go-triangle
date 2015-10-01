42dc554f57b3905e273b08df471922f0014f6364
Wasith Theerapattrathamrong - Acer <phai@live.com>
2015-10-01 15:24:44 +0700

    refacoring code to use const instead of hard code

--- triangle.go
+++ triangle.go
@@ -6,12 +6,15 @@ import (
 	"strconv"
 )
 
+const MAXIMUM_SIZE = 3
+const MAXIMUM_ARGRUMENTS = MAXIMUM_SIZE + 1
+
 func main() {
 	if ShowHelp() {
 		return
 	}
 
-	var sides [3]int
+	var sides [MAXIMUM_SIZE]int
 	var isInteger bool
 	if isInteger, sides = IsAllArgsAreInt(); !isInteger {
 		fmt.Println("[!] Please enter all 3 parameters with integer value.")
@@ -22,19 +25,24 @@ func main() {
 		fmt.Println("[!] All 3 argruments must be positive value.")
 		return
 	}
+
+	if IsAnyValueMoreThanMaximumRange(sides, 200) {
+		fmt.Println("[!] There is some value is out of range.")
+		return
+	}
 }
 
 func ShowHelp() bool {
-	if len(os.Args) != 4 {
+	if len(os.Args) != MAXIMUM_ARGRUMENTS {
 		fmt.Println("[I] Please enter 3 parameters for 3 sides of triangle.")
-		fmt.Println("  eg. " + os.Args[0] + " 3 4 5")
+		fmt.Println("    eg. " + os.Args[0] + " 3 4 5")
 		return true
 	}
 	return false
 }
 
-func IsAllArgsAreInt() (isTriangle bool, sides [3]int) {
-	for i := 1; i < 4; i++ {
+func IsAllArgsAreInt() (isTriangle bool, sides [MAXIMUM_SIZE]int) {
+	for i := 1; i < MAXIMUM_SIZE+1; i++ {
 		side, err := strconv.Atoi(os.Args[i])
 		if err != nil {
 			isTriangle = false
@@ -47,9 +55,18 @@ func IsAllArgsAreInt() (isTriangle bool, sides [3]int) {
 	return
 }
 
-func IsAnyNegativeValue(sides [3]int) bool {
+func IsAnyNegativeValue(sides [MAXIMUM_SIZE]int) bool {
 	for _, sideWidth := range sides {
 		if sideWidth < 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func IsAnyValueMoreThanMaximumRange(sides [MAXIMUM_SIZE]int, maximumValueValidable int) bool {
+	for _, sideWidth := range sides {
+		if sideWidth > maximumValueValidable {
 			return true
 		}
 	}
