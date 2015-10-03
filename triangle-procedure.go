e96f9350f3ceb6a7167a29760342f9910eb78e3b
Wasith Theerapattrathamrong - MBPr <phai@live.com>
2015-10-04 01:27:35 +0700

    be more simple to draw graph

--- triangle-procedure.go
+++ triangle-procedure.go
@@ -7,41 +7,27 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 4 {
-		fmt.Println("[I] Please enter 3 parameters for 3 sides of triangle.")
-		fmt.Println("    eg. " + os.Args[0] + " 3 4 5")
-		return
-	}
+	a := strconv.Atoi(os.Args[0])
+	b := strconv.Atoi(os.Args[1])
+	c := strconv.Atoi(os.Args[2])
 
-	var sides [3]int
-
-	for i := 1; i < 4; i++ {
-		side, err := strconv.Atoi(os.Args[i])
-		if err != nil {
-			fmt.Println("[!] Please enter all 3 parameters with integer value.")
-			return
-		} else {
-			sides[i-1] = side
-		}
-	}
-
-	if sides[0] < 0 || sides[1] < 0 || sides[2] < 0 {
+	if a < 0 || b < 0 || c < 0 {
 		fmt.Println("[!] All 3 argruments must be positive value.")
-	} else if sides[0] > 200 || sides[1] > 200 || sides[2] > 200 {
+	} else if a > 200 || b > 200 || c > 200 {
 		fmt.Println("[!] There is some value is out of range.")
-	} else if sides[0]+sides[1] <= sides[2] || sides[0]+sides[2] <= sides[1] || sides[1]+sides[2] <= sides[0] {
+	} else if a+b <= c || a+c <= b || b+c <= a {
 		fmt.Println("[!] Not a triangle.")
 	} else {
-		aPow2 := sides[0] * sides[0]
-		bPow2 := sides[1] * sides[1]
-		cPow2 := sides[2] * sides[2]
+		aPow2 := a * a
+		bPow2 := b * b
+		cPow2 := c * c
 		if aPow2+bPow2 == cPow2 || aPow2+cPow2 == bPow2 || bPow2+cPow2 == aPow2 {
 			fmt.Println("[A] Right Triangle.")
-		} else if sides[0] == sides[1] && sides[1] == sides[2] {
+		} else if a == b && b == c {
 			fmt.Println("[A] Equilateral.")
-		} else if sides[0] == sides[1] && sides[0]+sides[1] > sides[2] || sides[0] == sides[2] && sides[0]+sides[2] > sides[1] || sides[1] == sides[2] && sides[1]+sides[2] > sides[0] {
+		} else if a == b && a+b > c || a == c && a+c > b || b == c && b+c > a {
 			fmt.Println("[A] Isosceles.")
-		} else if sides[0] != sides[1] && sides[0] != sides[2] && sides[1] != sides[2] {
+		} else if a != b && a != c && b != c {
 			fmt.Println("[A] Scalene.")
 		}
 	}
